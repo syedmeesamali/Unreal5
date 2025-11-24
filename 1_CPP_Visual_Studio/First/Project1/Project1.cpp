44b5cd2f30d81b9d48f1c0a30d21c95cd8b5abef
syedmeesamali <syedmeesamali@yahoo.com>
2025-11-24 23:50:50 +0300

    Reviews continue

--- 1_CPP_Visual_Studio/First/Project1/Project1.cpp
+++ 1_CPP_Visual_Studio/First/Project1/Project1.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main()
 {
-    const int MinutesPerSecond{ 60 };
+    const int SecondsPerMin{ 60 };
     int TotalSecondsPlayed{ 100 };       //Pascal case naming convention + Braced initializer
-    int MinutesPlayed = TotalSecondsPlayed / MinutesPerSecond;
+    int MinutesPlayed = TotalSecondsPlayed / SecondsPerMin;
+    int SecondsPlayed = TotalSecondsPlayed % SecondsPerMin;
     cout << "Total minutes played: " << MinutesPlayed << endl;
+    cout << "Total seconds played: " << SecondsPlayed << endl;
 }
