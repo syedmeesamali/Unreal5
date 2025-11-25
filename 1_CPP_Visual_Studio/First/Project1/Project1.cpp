d8235be95f5ebedf46bb36040ed6912266500d54
syedmeesamali <syedmeesamali@yahoo.com>
2025-11-25 23:58:46 +0300

    Typecast in CPP

--- 1_CPP_Visual_Studio/First/Project1/Project1.cpp
+++ 1_CPP_Visual_Studio/First/Project1/Project1.cpp
@@ -11,4 +11,9 @@ int main()
     int SecondsPlayed = TotalSecondsPlayed % SecondsPerMin;
     cout << "Total minutes played: " << MinutesPlayed << endl;
     cout << "Total seconds played: " << SecondsPlayed << endl;
+
+    int Score{ 1360 };
+    int SecondsPlayedNew { 10000 };
+    float PointsPerSecond = static_cast<float> (Score) / SecondsPlayedNew;
+    cout << "Points per second: " << PointsPerSecond << endl;
 }
